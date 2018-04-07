9822f8587f49c63ad050ab203f4b07abf2f573ee
farkhad <farkhadcity@gmail.com>
2018-04-07 21:10:20 +0300

    webpack config for react html sass

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss'
+
+const Index = () => {
+    return <div className='text-block'>Helllo Galo</div>
+}
+
+ReactDOM.render(<Index></Index>, document.getElementById('index'))
